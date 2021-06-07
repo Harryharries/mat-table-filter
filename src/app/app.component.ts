a9a5316e86ac22304c1cbc51b04fe538355fd9f0
Harryharries <A1021604754@gmail.com>
2021-06-07 13:09:01 -0500

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,16 +13,17 @@ export const ELEMENT_DATA: PeriodicElement[] = [
   { position: 2, name: "Helium", weight: 4.0026, symbol: "He" },
   { position: 3, name: "Lithium", weight: 6.941, symbol: "Li" },
   { position: 4, name: "Beryllium", weight: 9.0122, symbol: "" },
-  { position: 5, name: "Boron", weight: 10.811, symbol: "B" },
+  { position: 5, name: "", weight: 10.811, symbol: "B" },
   { position: 6, name: "Carbon", weight: 12.0107, symbol: "C" },
-  { position: 7, name: "Nitrogen", weight: 14.0067, symbol: "" },
-  { position: 8, name: "Oxygen", weight: 15.9994, symbol: "O" },
+  { position: 7, name: "", weight: 14.0067, symbol: "" },
+  { position: 8, name: "", weight: 15.9994, symbol: "O" },
   { position: 9, name: "Fluorine", weight: 18.9984, symbol: "" },
-  { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" }
+  { position: 10, name: "Neon", weight: 20.1797, symbol: "Ne" },
+  { position: 11, name: "", weight: 15.9994, symbol: "O" },
+  { position: 12, name: "Fluorine", weight: 18.9984, symbol: "H" },
 ];
 
 export const CONDITIONS_LIST = [
-  { value: "nono", label: "Nono" },
   { value: "is-empty", label: "Is empty" },
   { value: "is-not-empty", label: "Is not empty" },
   { value: "is-equal", label: "Is equal" },
@@ -37,7 +38,7 @@ export const CONDITIONS_FUNCTIONS = { // search method base on conditions list v
     return value !== "";
   },
   "is-equal": function (value, filterdValue) {
-    return value == filterdValue;
+    return value.includes(filterdValue);
   },
   "is-not-equal": function (value, filterdValue) {
     return value != filterdValue;
@@ -63,14 +64,14 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.dataSource.filterPredicate = (p: PeriodicElement, filtre: any) => {
       let result = true;
-      let keys = Object.keys(p); // keys of the object data 
+      let keys = Object.keys(p); // keys of the object data
 
       for (const key of keys) {
         let searchCondition = filtre.conditions[key]; // get search filter method
 
         if (searchCondition && searchCondition !== 'none') {
-          if (filtre.methods[searchCondition](p[key], filtre.values[key]) === false) { // invoke search filter 
-            result = false // if one of the filters method not succeed the row will be remove from the filter result 
+          if (filtre.methods[searchCondition](p[key], filtre.values[key]) === false) { // invoke search filter
+            result = false // if one of the filters method not succeed the row will be remove from the filter result
             break;
           }
         }
